b28c40577daa6399e7458534eaf1d51e22d8669f
Judeotine <75664161+judeotine@users.noreply.github.com>
2025-11-15 10:32:25 +0300

    chore: add Next.js framework configuration file (next.config.ts)

---
+++ next.config.ts
@@ -0,0 +1,17 @@
+import type { NextConfig } from "next";
+import path from "path";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: path.join(__dirname),
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
+
+export default nextConfig;
